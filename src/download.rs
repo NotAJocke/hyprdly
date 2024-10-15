c3d22abf8a80649ec083990e361d01a8e77fa24e
NotAJocke <jockerider199@gmail.com>
2024-10-16 00:18:46 +0200

    refactor: move downloads fn to own mod

---
+++ src/download.rs
@@ -0,0 +1,49 @@
+use std::process::Command;
+
+pub fn make(args: Vec<String>) -> Result<String, String> {
+    let cmd_output = Command::new("yt-dlp")
+        .args(args)
+        .output()
+        .expect("Failed to exec process.");
+
+    let bytes: Vec<u8> = if cmd_output.status.success() {
+        cmd_output.stdout
+    } else {
+        cmd_output.stderr
+    };
+
+    let out = unsafe { String::from_utf8_unchecked(bytes) };
+    let out = out.to_string();
+
+    if cmd_output.status.success() {
+        Ok(out)
+    } else {
+        Err(out)
+    }
+}
+
+#[derive(Debug)]
+pub struct Output {
+    pub id: String,
+    pub title: String,
+    pub bytes: String,
+    pub duration: String,
+}
+
+pub fn parse_output(out: String) -> Vec<Output> {
+    let data: Vec<String> = out.lines().map(str::trim).map(String::from).collect();
+    let amount = data.len() / 4;
+
+    let mut outputs: Vec<Output> = Vec::with_capacity(amount);
+
+    for i in 0..amount {
+        outputs.push(Output {
+            id: data[i * 4].to_owned(),
+            title: data[i * 4 + 1].to_owned(),
+            bytes: data[i * 4 + 2].to_owned(),
+            duration: data[i * 4 + 3].to_owned(),
+        });
+    }
+
+    outputs
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::{
 use download_builder::*;
 use indicatif::DecimalBytes;
 
+mod download;
 mod download_builder;
 mod utils;
 
@@ -61,13 +62,13 @@ fn main() {
     spinner.enable_steady_tick(Duration::from_millis(100));
 
     let start = Instant::now();
-    let dl_result = download(dl_args);
+    let dl_result = download::make(dl_args);
     let end = start.elapsed();
 
     match dl_result {
         Ok(s) => {
             spinner.finish();
-            let outputs = parse_output(s);
+            let outputs = download::parse_output(s);
             let total_bytes = total_download_bytes(&outputs);
             let human_bytes = DecimalBytes(total_bytes);
             let human_time = human_duration(end);
@@ -83,47 +84,7 @@ fn main() {
     }
 }
 
-pub fn download(args: Vec<String>) -> Result<String, String> {
-    let cmd_output = Command::new("yt-dlp")
-        .args(args)
-        .output()
-        .expect("Failed to exec process.");
-
-    let bytes: Vec<u8> = if cmd_output.status.success() {
-        cmd_output.stdout
-    } else {
-        cmd_output.stderr
-    };
-
-    let out = unsafe { String::from_utf8_unchecked(bytes) };
-    let out = out.to_string();
-
-    if cmd_output.status.success() {
-        Ok(out)
-    } else {
-        Err(out)
-    }
-}
-
-pub fn parse_output(out: String) -> Vec<Output> {
-    let data: Vec<String> = out.lines().map(str::trim).map(String::from).collect();
-    let amount = data.len() / 4;
-
-    let mut outputs: Vec<Output> = Vec::with_capacity(amount);
-
-    for i in 0..amount {
-        outputs.push(Output {
-            id: data[i * 4].to_owned(),
-            title: data[i * 4 + 1].to_owned(),
-            bytes: data[i * 4 + 2].to_owned(),
-            duration: data[i * 4 + 3].to_owned(),
-        });
-    }
-
-    outputs
-}
-
-pub fn total_download_bytes(outputs: &[Output]) -> u64 {
+pub fn total_download_bytes(outputs: &[download::Output]) -> u64 {
     outputs
         .iter()
         .map(|e| &e.bytes)
@@ -139,12 +100,4 @@ pub fn human_duration(duration: Duration) -> String {
     } else {
         format!("{}s", duration.as_secs())
     }
-}
-
-#[derive(Debug)]
-pub struct Output {
-    pub id: String,
-    pub title: String,
-    pub bytes: String,
-    pub duration: String,
 }
